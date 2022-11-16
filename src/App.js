2bc189afe29fa530ce6bd11b81f9bf0d38761766
Gabriela Knorst Olson <gabriela.olson@icloud.com>
2022-11-16 12:01:47 -0300

    update React.dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-
-
-import './App.css';
-import { FileUpload } from './components/FileUpload';
-
-
-
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import { FileUpload } from "./components/FileUpload";
+import Profile from "./components/Profile";
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  
-  return (
-    <div className="App">
-      <FileUpload/>
-    </div>
+  const { isLoading, error, isAuthenticated } = useAuth0();
+  return isAuthenticated ? (
+    <>
+      <FileUpload />
+      <LogoutButton />
+    </>
+  ) : (
+    <main className="column">
+      <h1>Entrar</h1>
+      {error && <p>Erro de autenticação</p>}
+      {!error && isLoading && <p>Carregando...</p>}
+      {!error && !isLoading && (
+        <>
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+        </>
+      )}
+    </main>
   );
 }
 
---
+++ src/components/awsBucket.js
@@ -0,0 +1,22 @@
+// var albumBucketName = "BUCKET_NAME";
+// var bucketRegion = "REGION";
+// var IdentityPoolId = "IDENTITY_POOL_ID";
+
+// AWS.config.update({
+//   region: bucketRegion,
+//   credentials: new AWS.CognitoIdentityCredentials({
+//     IdentityPoolId: IdentityPoolId,
+//   }),
+// });
+
+// var s3 = new AWS.S3({
+//   apiVersion: "2006-03-01",
+//   params: { Bucket: albumBucketName },
+// });
+
+// AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+//   IdentityPoolId: "IDENTITY_POOL_ID",
+//   Logins: {
+//     "login.provider.com": token,
+//   },
+// });
