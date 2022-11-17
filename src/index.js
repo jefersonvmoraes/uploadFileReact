92470561e146c8a7d4fb15a6dc43c4cdcf9939c8
Dalton Teixeira <dalton.silveira@gmail.com>
2022-11-17 11:59:05 -0300

    Added bootstrap back

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
-import reportWebvitals from "./reportWebVitals";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Auth0Provider
-//       domain={domain}
-//       clientId={clientId}
-//       redirectUri={window.location.origin}
-//     >
-//       <App />
-//     </Auth0Provider>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -30,9 +15,7 @@ root.render(
       clientId={clientId}
       redirectUri={window.location.origin}
     >
-      <App />
+    <App />
     </Auth0Provider>
   </React.StrictMode>
 );
-
-reportWebvitals();
